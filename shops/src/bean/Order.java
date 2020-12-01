ef8780cb8779dffdd0c66cfd31ba2c707cd0e3ef
ry19304038 <ry19304038@ga.tera-house.ac.jp>
2020-12-01 14:05:31 +0900

    commit

--- shops/src/bean/Order.java
+++ shops/src/bean/Order.java
@@ -1,5 +1,45 @@
 package bean;
 
-public class Order {
+import java.io.Serializable;
+
+public class Order implements Serializable{
+	private String orderId;
+	private String userId;
+	private String buyDate;
+	private int price;
+	private int itemCount;
+
+
+	public String getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getBuyDate() {
+		return buyDate;
+	}
+	public void setBuyDate(String buyDate) {
+		this.buyDate = buyDate;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getItemCount() {
+		return itemCount;
+	}
+	public void setItemCount(int itemCount) {
+		this.itemCount = itemCount;
+	}
+
 
 }
--- shops/src/bean/OrderDetail.java
+++ shops/src/bean/OrderDetail.java
@@ -1,5 +1,29 @@
 package bean;
 
-public class OrderDetail {
+import java.io.Serializable;
+
+public class OrderDetail implements Serializable{
+	private String orderId;
+	private String itemId;
+	private int buyCount;
+	public String getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+	public String getItemId() {
+		return itemId;
+	}
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+	public int getBuyCount() {
+		return buyCount;
+	}
+	public void setBuyCount(int buyCount) {
+		this.buyCount = buyCount;
+	}
+
 
 }
