2e14047a3e9c5db0ca3bda99e89062410ced6e48
kamuraaya <ak19304010@ga.tera-house.ac.jp>
2020-12-01 14:00:14 +0900

    おすのだ

--- shops/src/bean/Item.java
+++ shops/src/bean/Item.java
@@ -1,5 +1,63 @@
 package bean;
 
-public class Item {
+import java.io.Serializable;
 
+public class Item implements Serializable{
+	private String itemId;
+	private String itemName;
+	private int stockCount;
+	private String sizeId;
+	private String colorId;
+	private int price;
+	private String categoryId;
+	private String detail;
+
+	public String getItemId() {
+		return itemId;
+	}
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public int getStockCount() {
+		return stockCount;
+	}
+	public void setStockCount(int stockCount) {
+		this.stockCount = stockCount;
+	}
+	public String getSizeId() {
+		return sizeId;
+	}
+	public void setSizeId(String sizeId) {
+		this.sizeId = sizeId;
+	}
+	public String getColorId() {
+		return colorId;
+	}
+	public void setColorId(String colorId) {
+		this.colorId = colorId;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
 }
