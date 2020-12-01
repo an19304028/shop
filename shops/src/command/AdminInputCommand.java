911483af0d69edf7bc5f2666a71e3d6cf1293261
ayu2000 <an19304028@ga.tera-house.ac.jp>
2020-12-01 14:26:45 +0900

    a

--- shops/src/command/AdminInputCommand.java
+++ shops/src/command/AdminInputCommand.java
@@ -1,10 +1,8 @@
 package command;
 
-import java.util.HashMap;
-
 import presentation.ResponseContext;
 
-public class InputCommand extends AbstractCommand {
+public class AdminInputCommand extends AbstractCommand {
 	public ResponseContext execute(ResponseContext resc) {
 	
 		resc.setTarget("adminjsp/Admin");
