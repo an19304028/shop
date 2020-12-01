4eb83ff5bbf3642d738ac433baf322fa3bfb3195
futabakumada <fk.usg.yo4@icloud.com>
2020-12-01 14:07:18 +0900

    push

--- shops/src/bean/Category.java
+++ shops/src/bean/Category.java
@@ -1,5 +1,20 @@
 package bean;
 
-public class Category {
+import java.io.Serializable;
 
+public class Category implements Serializable {
+	private String categoryId;
+	private String categoryName;
+	public String getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
 }
--- shops/src/bean/image.java
+++ shops/src/bean/image.java
@@ -1,5 +1,28 @@
 package bean;
 
-public class image {
+import java.io.Serializable;
+
+public class image implements Serializable{
+	private String imageId;
+	private String imagePath;
+	private String itemId;
+	public String getImageId() {
+		return imageId;
+	}
+	public void setImageId(String imageId) {
+		this.imageId = imageId;
+	}
+	public String getImagePath() {
+		return imagePath;
+	}
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	public String getItemId() {
+		return itemId;
+	}
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
 
 }
