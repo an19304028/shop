36bd1aa60182a74fa4c901c1a3191b059585d727
no19304008 <no19304008@ga.tera-house.ac.jp>
2020-12-01 16:26:26 +0900

    Dao大橋

--- shops/src/dao/OrderDao.java
+++ shops/src/dao/OrderDao.java
@@ -1,5 +1,11 @@
 package dao;
 
-public class OrderDao {
+import java.util.List;
 
+import bean.Order;
+
+public interface OrderDao {
+	public abstract void addOrder(Order o);
+	public Order getOrder(String orderId);
+	public List<?> getAllOrders();
 }
