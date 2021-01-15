82eca3dba3b1c06a23a314ace9357fef21d66f12
ry19304038 <ry19304038@ga.tera-house.ac.jp>
2021-01-15 15:39:43 +0900

    o

--- shops/src/dao/order/MySQLRemoveCartDao.java
+++ shops/src/dao/order/MySQLRemoveCartDao.java
@@ -1,6 +1,10 @@
 package dao.order;
 
+import bean.Order;
+
 public class MySQLRemoveCartDao implements RemoveCartDao{
+
+	public void removeCart(Order o) {}
 
 
 }
--- shops/src/dao/order/RemoveCartDao.java
+++ shops/src/dao/order/RemoveCartDao.java
@@ -1,4 +1,4 @@
-package dao;
+package dao.order;
 
 import bean.Order;
 
