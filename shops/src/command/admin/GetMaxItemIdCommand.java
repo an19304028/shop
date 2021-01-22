a620d4b018d2bc55639f4e858ed4d2eda1e5bed0
ayu2000 <an19304028@ga.tera-house.ac.jp>
2021-01-22 15:56:35 +0900

    command

---
+++ shops/src/command/admin/GetMaxItemIdCommand.java
@@ -0,0 +1,22 @@
+package command.admin;
+
+import java.util.List;
+
+import command.AbstractCommand;
+import dao.admin.ItemDao;
+import daofactory.AbstractDaoFactory;
+import presentation.ResponseContext;
+
+public class GetMaxItemIdCommand extends AbstractCommand {
+	public ResponseContext execute(ResponseContext resc) {
+		AbstractDaoFactory factory = AbstractDaoFactory.getFactory();
+		ItemDao dao = factory.getItemDao();  
+		
+		List maxId = dao.getMaxItemId();	
+		resc.setResult(maxId);
+		resc.setTarget("/WEB-INF/adminjsp/Admin.jsp");
+		System.out.println("target:"+resc.getTarget());
+		return resc;
+	}
+
+}
