093851089a094c7294205ccb88561692857902b2
ss19304021 <ss19304021@ga.tera-house.ac.jp>
2021-01-22 16:29:34 +0900

    s

--- shops/src/dao/favorite/MySQLAddFavoDao.java
+++ shops/src/dao/favorite/MySQLAddFavoDao.java
@@ -1,13 +1,34 @@
 package dao.favorite;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import bean.Favorite;
+import daofactory.Connector;
 
 public class MySQLAddFavoDao implements AddFavoDao{
+	private PreparedStatement st = null;
 
 	@Override
 	public void addFavo(Favorite f) {
 		// TODO 自動生成されたメソッド・スタブ
+		try {
+			Connection cn = Connector.connect();
+			String sql = "INSERT into shop.favo_table VALUES(?,?)";
 
+			st = cn.prepareStatement(sql);
+
+			st.setString(1, f.getUserId());
+			st.setString(2, f.getItemId());
+
+			st.executeUpdate();
+
+			cn.commit();
+			cn.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- shops/src/dao/favorite/MySQLGetFavoListDao.java
+++ shops/src/dao/favorite/MySQLGetFavoListDao.java
@@ -1,13 +1,40 @@
 package dao.favorite;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import bean.Favorite;
+import daofactory.Connector;
+
 public class MySQLGetFavoListDao implements GetFavoListDao{
+	private PreparedStatement st = null;
 
 	@Override
 	public List getFavoList() {
 		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		ArrayList favo = new ArrayList();
+		try {
+			Connection cn = Connector.connect();
+
+			String sql = "SELECT user_id,item_id FROM shop.favo_table ";
+			st = cn.prepareStatement(sql);
+
+			ResultSet rs = st.executeQuery();
+			while(rs.next()) {
+				Favorite f = new Favorite();
+				f.setUserId(rs.getString(1));
+				f.setItemId(rs.getString(2));
+
+				favo.add(f);
+			}
+			cn.close();
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		return favo;
 	}
 
 }
