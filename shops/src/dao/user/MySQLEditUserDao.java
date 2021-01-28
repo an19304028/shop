2ca53d86567fe5994a4d5a5fcbea27285b0d5ebd
ry19304038 <ry19304038@ga.tera-house.ac.jp>
2021-01-28 17:12:50 +0900

    Edit

--- shops/src/dao/user/MySQLEditUserDao.java
+++ shops/src/dao/user/MySQLEditUserDao.java
@@ -19,12 +19,12 @@ public class MySQLEditUserDao implements EditUserDao{
 
 			st = cn.prepareStatement(sql);
 
-			st.setString(1, u.getName());
-			st.setString(2, u.getKana());
+			//st.setString(1, u.getName());
+			//st.setString(2, u.getKana());
 			st.setString(3, u.getMail());
 			st.setString(4, u.getLoginId());
 			//st.setString(5, u.getPassword());
-			st.setString(6, u.getGender());
+			//st.setString(6, u.getGender());
 			//st.setString(7, u.getBirthday());
 			st.setString(8, u.getTell());
 			st.setString(9, u.getPostalCode());
