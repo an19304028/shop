a7215f83be39c788d1dae2438371da98c6383f59
ry19304038 <ry19304038@ga.tera-house.ac.jp>
2021-01-29 11:55:37 +0900

    ?

--- shops/src/dao/user/MySQLGetCreditDao.java
+++ shops/src/dao/user/MySQLGetCreditDao.java
@@ -2,22 +2,36 @@ package dao.user;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import bean.User;
 import daofactory.Connector;
 
 public class MySQLGetCreditDao implements GetCreditDao {
 	private PreparedStatement st = null;
 	public void getCredit(String userId) {
+		ArrayList user = new ArrayList();
 		try {
 			Connection cn = Connector.connect();
-			String sql = "SELECT FROM shop.credit_table WHERE user_id=?";
+			String sql = "SELECT * FROM shop.credit_table WHERE user_id=?";
 			st = cn.prepareStatement(sql);
 			st.setString(1,userId);
 
-			st.executeUpdate();
+			ResultSet rs = st.executeQuery();
+			while(rs.next()) {
+				User u = new User();
 
-			cn.commit();
+				u.setUserId(rs.getString(1));
+				u.setCreditNumber(rs.getString(2));
+				u.setSecurtyCode(rs.getString(3));
+				u.setExpirationCode(rs.getString(4));
+				u.setPayCount(rs.getInt(5));
+
+
+				user.add(u);
+
 			cn.close();
 		}catch(SQLException e) {
 			e.printStackTrace();
