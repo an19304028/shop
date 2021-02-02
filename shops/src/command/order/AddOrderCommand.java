9029e4007c721cda82113b0e75fc58d2b468bdb9
kamuraaya <ak19304010@ga.tera-house.ac.jp>
2021-02-02 13:32:21 +0900

    AddOrderCommand

--- shops/src/command/order/AddOrderCommand.java
+++ shops/src/command/order/AddOrderCommand.java
@@ -1,5 +1,7 @@
 package command.order;
 
+import java.util.ArrayList;
+
 import bean.Order;
 import bean.OrderDetail;
 import command.AbstractCommand;
@@ -14,28 +16,35 @@ public class AddOrderCommand extends AbstractCommand{
 	public ResponseContext execute(ResponseContext resc) {
 		RequestContext rc = getRequestContext();
 
-		//変更するかもしれない
 		String userId =rc.getParameter("userId")[0];
-		String itemId= rc.getParameter("itemId")[0];
+		//String itemId= rc.getParameter("itemId")[0];
 		int price = Integer.parseInt(rc.getParameter("price")[0]);
 		int itemCount = Integer.parseInt(rc.getParameter("itemCount")[0]);
-		int buyCount = Integer.parseInt(rc.getParameter("buyCount")[0]);
-		System.out.println(userId + "\t"+itemId + "\t"+price + "\t"+itemCount);
+		//int buyCount = Integer.parseInt(rc.getParameter("buyCount")[0]);
+		System.out.println(userId + "\t"+price + "\t"+itemCount);
 
+		ArrayList list = new ArrayList();
+		//for文でparameter全部引っ張り出す
+		for(int i = 0; i< rc.getParameter("itemId").length; i++) {
+			OrderDetail od = new OrderDetail();
+			od.setItemId(rc.getParameter("itemId")[i]);
+			od.setBuyCount(Integer.parseInt(rc.getParameter("buyCount")[i]));
+
+			list.add(od);
+
+			System.out.println("item_id:"+rc.getParameter("itemId")[i]+"\tbuy_count:"+rc.getParameter("buyCount")[i]);
+		}
 
 		Order o = new Order();
-		OrderDetail od = new OrderDetail();
 
 		o.setUserId(userId);
 		o.setPrice(price);
 		o.setItemCount(itemCount);
-		od.setItemId(itemId);
-		od.setBuyCount(buyCount);
 
 		AbstractDaoFactory factory = AbstractDaoFactory.getFactory();
 		AddOrderDao dao = factory.getAddOrderDao();
 
-		dao.addOrder(o);
+		dao.addOrder(o, list);
 
 		resc.setMess("注文情報を確定いたしました。");
 
