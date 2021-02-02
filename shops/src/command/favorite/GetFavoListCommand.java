01c8b884b2ca5a98e543130b3ce7c703e629ee33
futabakumada <fk.usg.yo4@icloud.com>
2021-02-02 14:41:55 +0900

    favo

--- shops/src/command/favorite/GetFavoListCommand.java
+++ shops/src/command/favorite/GetFavoListCommand.java
@@ -1,5 +1,31 @@
 package command.favorite;
 
-public class GetFavoListCommand {
+import bean.Favorite;
+import command.AbstractCommand;
+import dao.favorite.GetFavoListDao;
+import daofactory.AbstractDaoFactory;
+import presentation.RequestContext;
+import presentation.ResponseContext;
 
+public class GetFavoListCommand extends AbstractCommand{
+	public ResponseContext execute(ResponseContext resc) {
+		RequestContext rc = getRequestContext();
+
+		String userId = rc.getParameter("userId")[0];
+		String itemId = rc.getParameter("itemId")[0];
+
+		Favorite f = new Favorite();
+
+		f.setUserId(userId);
+		f.setItemId(itemId);
+
+		AbstractDaoFactory factory = AbstractDaoFactory.getFactory();
+		GetFavoListDao dao = factory.getGetFavoListDao();
+
+		dao.getFavoList(f);
+
+		resc.setTarget("/WEB-INF/userjsp/FavoList.jsp");
+		System.out.println("target:"+resc.getTarget());
+		return resc;
+	}
 }
