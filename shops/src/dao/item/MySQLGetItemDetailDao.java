18e71d89d4bd12d85ddb2a2f0d952a3a47b345c0
ayu2000 <an19304028@ga.tera-house.ac.jp>
2021-02-02 11:35:40 +0900

    item

--- shops/src/dao/item/MySQLGetItemDetailDao.java
+++ shops/src/dao/item/MySQLGetItemDetailDao.java
@@ -20,20 +20,24 @@ public class MySQLGetItemDetailDao implements GetItemDetailDao{
 		try {
 			Connection cn = Connector.connect();
 
-			String sql = "SELECT item_id, item_name, stock_count, size_id, color_id, price, category_id, detail, image_path From shop.item_table";
+			String sql = "SELECT i.item_id, i.item_name, i.stock_count, s.size_name ,c.color_name, ca.category_name, i.price, i.detail FROM shop.item_table i JOIN shop.size_table s USING(size_id) JOIN shop.color_table c USING(color_id) JOIN shop.category_table ca USING(category_id) Where item_id=?";
 			st = cn.prepareStatement(sql);
-
+			
+			st.setString(1,key);
+			
 			ResultSet rs = st.executeQuery();
+			
 			while(rs.next()) {
 				Item i = new Item();
 				i.setItemId(rs.getString(1));
 				i.setItemName(rs.getString(2));
 				i.setStockCount(rs.getInt(3));
-				i.setSizeId(rs.getString(4));
-				i.setColorId(rs.getString(5));
-				i.setCategoryId(rs.getString(6));
-				i.setDetail(rs.getString(7));
-				i.setImagePath(rs.getString(8));
+				i.setSizeName(rs.getString(4));
+				i.setColorName(rs.getString(5));
+				i.setCategoryName(rs.getString(6));
+				i.setPrice(rs.getInt(7));
+				i.setDetail(rs.getString(8));
+				//i.setImagePath(rs.getString(9));
 
 				item.add(i);
 			}
