7a904a2e1bb820caab481cadd10fe5b373cd6311
ry19304038 <ry19304038@ga.tera-house.ac.jp>
2021-02-03 16:19:05 +0900

    ｆ

--- shops/src/bean/Favorite.java
+++ shops/src/bean/Favorite.java
@@ -5,6 +5,17 @@ import java.io.Serializable;
 public class Favorite implements Serializable{
 	private String userId;
 	private String itemId;
+	private String itemName;
+	private int stockCount;
+	private String sizeId;
+	private String colorId;
+	private int price;
+	private String categoryId;
+	private String detail;
+	private String imagePath;
+	private String sizeName;
+	private String categoryName;
+	private String colorName;
 	public Favorite() {}
 
 	public String getUserId() {
@@ -18,6 +29,94 @@ public class Favorite implements Serializable{
 	}
 	public void setItemId(String itemId) {
 		this.itemId = itemId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public int getStockCount() {
+		return stockCount;
+	}
+
+	public void setStockCount(int stockCount) {
+		this.stockCount = stockCount;
+	}
+
+	public String getSizeId() {
+		return sizeId;
+	}
+
+	public void setSizeId(String sizeId) {
+		this.sizeId = sizeId;
+	}
+
+	public String getColorId() {
+		return colorId;
+	}
+
+	public void setColorId(String colorId) {
+		this.colorId = colorId;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
+	public String getSizeName() {
+		return sizeName;
+	}
+
+	public void setSizeName(String sizeName) {
+		this.sizeName = sizeName;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getColorName() {
+		return colorName;
+	}
+
+	public void setColorName(String colorName) {
+		this.colorName = colorName;
 	}
 
 }
--- shops/src/dao/favorite/MySQLGetFavoListDao.java
+++ shops/src/dao/favorite/MySQLGetFavoListDao.java
@@ -15,19 +15,28 @@ public class MySQLGetFavoListDao implements GetFavoListDao{
 
 	@Override
 	public List getFavoList() {
-		// TODO 自動生成されたメソッド・スタブ
 		ArrayList favo = new ArrayList();
 		try {
 			Connection cn = Connector.connect();
 
-			String sql = "SELECT user_id,item_id FROM shop.favo_table ";
+			String sql = "SELECT i.item_id, i.item_name, i.stock_count, s.size_name ,c.color_name, i.price, ca.category_name, i.detail, im.image_path FROM shop.item_table i JOIN shop.size_table s USING(size_id) JOIN shop.color_table c USING(color_id) JOIN shop.favo_table ca USING(category_id) JOIN shop.image_table im USING(item_id) WHERE userId=?";
 			st = cn.prepareStatement(sql);
 
 			ResultSet rs = st.executeQuery();
 			while(rs.next()) {
 				Favorite f = new Favorite();
-				f.setUserId(rs.getString(1));
-				f.setItemId(rs.getString(2));
+				f.setItemId(rs.getString(1));
+				f.setItemName(rs.getString(2));
+				f.setStockCount(rs.getInt(3));
+				f.setSizeName(rs.getString(4));
+				f.setColorName(rs.getString(5));
+				f.setPrice(rs.getInt(6));
+				f.setCategoryName(rs.getString(7));
+				f.setDetail(rs.getString(8));
+				//f.setImagePath(rs.getString(9));
+
+				System.out.println(f.getItemId()+f.getCategoryName()+f.getSizeName());
+
 
 				favo.add(f);
 			}
