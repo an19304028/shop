712fda38e680631dc96d44cf64f47ea25e56ef9a
kamuraaya <ak19304010@ga.tera-house.ac.jp>
2021-02-09 11:30:52 +0900

    EditUser

--- shops/src/command/user/EditUserCommand.java
+++ shops/src/command/user/EditUserCommand.java
@@ -1,5 +1,44 @@
 package command.user;
 
-public class EditUserCommand{
+import bean.User;
+import command.AbstractCommand;
+import dao.user.EditUserDao;
+import daofactory.AbstractDaoFactory;
+import presentation.RequestContext;
+import presentation.ResponseContext;
+
+public class EditUserCommand extends AbstractCommand{
+	public ResponseContext execute(ResponseContext resc) {
+		RequestContext rc = getRequestContext();
+		String mail = rc.getParameter("mail")[0];
+		String loginId = rc.getParameter("loginId")[0];
+		String password = rc.getParameter("password")[0];
+		String tell = rc.getParameter("tell")[0];
+		String postalCode = rc.getParameter("postalCode")[0];
+		String address = rc.getParameter("address")[0];
+		String userId = rc.getParameter("userId")[0];
+
+		System.out.println("UserId:"+userId+"の情報を編集");
+
+		User u = new User();
+		u.setMail(mail);
+		u.setLoginId(loginId);
+		u.setPassword(password);
+		u.setTell(tell);
+		u.setPostalCode(postalCode);
+		u.setAddress(address);
+		u.setUserId(userId);
+
+		AbstractDaoFactory factory = AbstractDaoFactory.getFactory();
+		EditUserDao dao = factory.getEditUserDao();
+		dao.editUser(u);
+		rc.setAttribute("mess", "変更しました");
+
+		resc.setTarget("getuser");
+		System.out.println("target:"+resc.getTarget());
+
+		return resc;
+	}
+
 
 }
--- shops/src/dao/user/MySQLEditUserDao.java
+++ shops/src/dao/user/MySQLEditUserDao.java
@@ -15,22 +15,18 @@ public class MySQLEditUserDao implements EditUserDao{
 		try {
 			Connection cn = Connector.connect();
 
-			String sql = "UPDATE shop.user_table SET name=?, kana=?, mail=?, login_id=?, password=?, gender=?, birthday=?, tell=?, postal_code=?, address=? WHERE = user_Id=?";
+			String sql = "UPDATE shop.user_table SET mail=?, login_id=?, password=?, tell=?, postal_code=?, address=? WHERE = user_Id=?";
 
 			st = cn.prepareStatement(sql);
 
-			//st.setString(1, u.getName());
-			//st.setString(2, u.getKana());
-			st.setString(3, u.getMail());
-			st.setString(4, u.getLoginId());
-			//st.setString(5, u.getPassword());
-			//st.setString(6, u.getGender());
-			//st.setString(7, u.getBirthday());
-			st.setString(8, u.getTell());
-			st.setString(9, u.getPostalCode());
-			st.setString(10, u.getAddress());
+			st.setString(1, u.getMail());
+			st.setString(2, u.getLoginId());
+			st.setString(3, u.getPassword());
+			st.setString(4, u.getTell());
+			st.setString(5, u.getPostalCode());
+			st.setString(6, u.getAddress());
 
-			st.setString(11, u.getUserId());
+			st.setString(7, u.getUserId());
 
 			st.executeUpdate();
 
