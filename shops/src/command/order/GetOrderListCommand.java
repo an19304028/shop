a9c1db7e7c27ebef3610ead6f37b973bd763e71e
ayu2000 <an19304028@ga.tera-house.ac.jp>
2021-02-10 14:47:33 +0900

    order

--- shops/src/command/order/GetOrderListCommand.java
+++ shops/src/command/order/GetOrderListCommand.java
@@ -5,6 +5,7 @@ import java.util.List;
 import command.AbstractCommand;
 import dao.order.GetOrderListDao;
 import daofactory.AbstractDaoFactory;
+import presentation.RequestContext;
 import presentation.ResponseContext;
 
 public class GetOrderListCommand extends AbstractCommand{
@@ -12,11 +13,12 @@ public class GetOrderListCommand extends AbstractCommand{
 	@Override
 	public ResponseContext execute(ResponseContext resc) {
 		AbstractDaoFactory factory = AbstractDaoFactory.getFactory();
+		RequestContext rc = getRequestContext();
+		String userId = (String) rc.getSessonAttribute("userId");
 		GetOrderListDao dao = factory.getGetOrderListDao();
 
-		List orders = dao.getOrderList();
-		System.out.println(dao.getOrderList());
-
+		List orders = dao.getOrderList(userId);
+		System.out.println(orders);
 		resc.setResult(orders);
 
 		resc.setTarget("/WEB-INF/adminjsp/Purchase.jsp");
