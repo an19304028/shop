f460bde18ab3b9fa4503e4e07e33929e6cf80fa0
ayu2000 <an19304028@ga.tera-house.ac.jp>
2021-03-02 12:14:51 +0900

    ユーザー削除

--- shops/src/command/user/RemoveUserCommand.java
+++ shops/src/command/user/RemoveUserCommand.java
@@ -9,7 +9,7 @@ import presentation.ResponseContext;
 public class RemoveUserCommand extends AbstractCommand{
 	public ResponseContext execute(ResponseContext resc){
 		RequestContext rc = getRequestContext();
-		String userId = (String)rc.getSessionAttribute("userId");
+		String userId = rc.getParameter("userId")[0];
 
 		AbstractDaoFactory factory = AbstractDaoFactory.getFactory();
 		RemoveUserDao dao = factory.getRemoveUserDao();
